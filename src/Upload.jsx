9ef3886a2f4cf96218ab25d99685be7cd18e3a13
Akshaj <akshaj.kanumuri@gmail.com>
2024-09-02 18:00:00 -0700

    got rudimentary upload system working

--- src/Upload.jsx
+++ src/Upload.jsx
@@ -1,7 +1,73 @@
-import React, {useState} from "react";
+import React, {useContext, useState} from "react";
 import { useNavigate, Link } from "react-router-dom";
+import {db, storage} from "./firebase"
+import {v4 as uuid} from "uuid";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import Attach from "./img/paper-clip.png"
 
 
 const Upload = () => {
-    
+    const [images, setImages] = useState([]);
+
+
+    const handleSend = async() => {
+
+        if (images.length == 0) {
+            console.log("No Files selected.")
+            return;
+        }
+        
+        images.forEach((img) => {
+        const storageRef = ref(storage, uuid());
+        const uploadTask = uploadBytesResumable(storageRef, img);
+
+        uploadTask.on(
+            "state_changed", // State changes (progress, etc.)
+            (snapshot) => {
+                // You can monitor progress here if needed
+            },
+            (error) => {
+                console.error("Upload failed:", error);
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    console.log("File available at:", downloadURL);
+                    // You can handle the download URL further here
+                });
+            }
+        );})
+    }
+
+    return(
+        <div className="input">
+            <div className="send" style={{paddingTop: "100px", textAlign: "center" }}>
+                {/* <img src={Attach} alt="Attach" /> */}
+                <input
+                    type="file"
+                    multiple
+                    style={{ display: "none" }}
+                    id="file"
+                    onChange={(e) => setImages([...e.target.files])}
+                />
+                <label htmlFor="file">
+                    {/* If img is selected, display a preview */}
+                    {images.length > 0 ? (
+                        images.map((img, index) => (
+                            <img
+                                key = {index}
+                                src={URL.createObjectURL(img)}
+                                alt={`Selected ${index}`}
+                                style={{width : 50, height : 50, marginRight : 5}}
+                            />
+                        ))
+                    ) : (
+                        <img src={Attach} alt="Attach" />
+                    )}
+                </label>
+                <button onClick={handleSend}>Upload</button>
+            </div>
+        </div>
+    )
 }
+
+export default Upload
