dd04e383aefdc5b601e924634c29014ba60d850d
kay singhal <130114612+iamkay556@users.noreply.github.com>
2024-09-09 00:04:35 -0700

    register login auth done; connects to firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+
 import Upload from "./Upload";
 import Login from "./components/login";
+import Home from "./components/home"
+import Register from "./components/register"
+
+import { AuthProvider } from './context/AuthContext';
+import { useRoutes } from 'react-router-dom';
+
 
 function App() {
+  const routesArray = [
+    {
+      path: "*",
+      element: <Login />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/home",
+      element: <Home />,
+    },
+  ];
+  let routesElement = useRoutes(routesArray);
   return (
     <div className="App">
-      <Login/>
-      {/* <header className="App-header">
+      <AuthProvider>
+        <Login/>
+        <div className="w-full h-screen flex flex-col">{routesElement}</div>
+        {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -20,7 +48,9 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
+      </header
+  > */}
+      </AuthProvider>
     </div>
   );
 }
