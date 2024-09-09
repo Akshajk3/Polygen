efb7593a604f5592724e8a830e40bb9f7d3643d5
Akshaj <akshaj.kanumuri@gmail.com>
2024-09-09 00:12:19 -0700

    fixed login issue

--- src/App.js
+++ src/App.js
@@ -33,23 +33,7 @@ function App() {
   return (
     <div className="App">
       <AuthProvider>
-        <Login/>
         <div className="w-full h-screen flex flex-col">{routesElement}</div>
-        {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header
-  > */}
       </AuthProvider>
     </div>
   );
